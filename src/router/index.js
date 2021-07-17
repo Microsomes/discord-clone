290ddaf17e9672f0019f077605d0e599ff3a0514
microsomes <tayyabdev@outlook.com>
2021-07-17 14:47:10 +0100

    n

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import GuildChatUI from '../ChatUI/discord/guildchat'
 import Emoji from  '../ChatUI/discord/emoji'
+import DiscordLogin from  '../ChatUI/discord/login'
+import DiscordRegister from  '../ChatUI/discord/register'
+
 
 const routes = [
+  {
+    path:"/register",
+    component:DiscordRegister
+  },
+  {
+    path:"/login",
+    component:DiscordLogin
+  },
   {
     path:"/emoji",
     component:Emoji
